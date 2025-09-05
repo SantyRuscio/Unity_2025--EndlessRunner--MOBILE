fd5fdef3ff1feb5b03675a1d97334ec4ade8cdee
SantyRuscio <santiruscio1237@gmail.com>
2025-09-05 19:11:52 -0300

    Arreglos , Creacion de CLASES
    Clase padre Item , hijos de item e hijso de los hijos

---
+++ Assets/Scripts/Item/Coin.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : Item
+{
+    //Variables para coin
+    public override void Execute() //Lojica de coin
+    {
+       
+    }
+
+}
