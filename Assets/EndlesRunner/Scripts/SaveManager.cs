546416382dce3a5914942152a3c2f33cd5c50ebe
Kodokusiano <uli.beghin@hotmail.com>
2025-10-30 21:12:50 -0300

    logica de creacion JSon (sin aplicar)

---
+++ Assets/EndlesRunner/Scripts/SaveManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class SaveManager
+{
+    private static string path = Application.persistentDataPath + "/save.json";
+
+    public static void Save(SaveData data)
+    {
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(path, json);
+    }
+
+    public static SaveData Load()
+    {
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            return JsonUtility.FromJson<SaveData>(json);
+        }
+        else
+        {
+            // si no hay uno, se crea
+            return new SaveData();
+        }
+    }
+}
